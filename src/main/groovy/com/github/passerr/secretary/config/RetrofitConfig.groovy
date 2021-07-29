9330db0b7e31fc89f0de188abdc66ec25299f699
PasseRR <xie__hai@sina.com>
2021-07-29 13:06:43 +0800

    http日志拦截

--- src/main/groovy/com/github/passerr/secretary/config/RetrofitConfig.groovy
+++ src/main/groovy/com/github/passerr/secretary/config/RetrofitConfig.groovy
@@ -1,6 +1,5 @@
 package com.github.passerr.secretary.config
 
-
 import com.github.passerr.secretary.api.BaiduTranslateApi
 import com.github.passerr.secretary.api.CoolQApi
 import com.github.passerr.secretary.api.GitlabApi
@@ -8,12 +7,11 @@ import com.github.passerr.secretary.api.ItpkApi
 import com.github.passerr.secretary.api.ItpkSettingApi
 import com.github.passerr.secretary.api.JiraApi
 import com.google.gson.Gson
-import groovy.util.logging.Slf4j
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.Response
-import okio.Buffer
+import okhttp3.logging.HttpLoggingInterceptor
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
@@ -21,15 +19,12 @@ import org.springframework.context.annotation.Configuration
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-import java.nio.charset.Charset
-
 /**
  * retrofit配置
  * @author xiehai* @date 2018/12/04 16:44
  * @Copyright (c) tellyes tech. inc. co.,ltd
  */
 @Configuration
-@Slf4j
 class RetrofitConfig {
     @Value("\${secretary.cool.url}")
     String coolQUrl
@@ -54,67 +49,55 @@ class RetrofitConfig {
 
     @Bean
     OkHttpClient coolHttpClient() {
-        new OkHttpClient.Builder().addInterceptor(new Interceptor() {
-            @Override
-            Response intercept(Interceptor.Chain chain) throws IOException {
-                // 添加授权token
-                Request request = chain.request()
-                                       .newBuilder()
-                                       .addHeader("Content-Type", "application/json")
-                                       .addHeader("Authorization", "Token ${RetrofitConfig.this.coolQToken}")
-                                       .build()
-
-                log.debug("${request.headers()}")
-                // post日志记录
-                if (request.body()) {
-                    Buffer buffer = new Buffer()
-                    request.body().writeTo(buffer)
-                    log.debug(buffer.readString(Charset.forName("UTF-8")))
+        new OkHttpClient.Builder()
+            .addInterceptor(new Interceptor() {
+                @Override
+                Response intercept(Interceptor.Chain chain) throws IOException {
+                    // 添加授权token
+                    Request request = chain.request()
+                        .newBuilder()
+                        .addHeader("Content-Type", "application/json")
+                        .addHeader("Authorization", "Token ${RetrofitConfig.this.coolQToken}")
+                        .build()
+                    chain.proceed(request)
                 }
-
-                chain.proceed(request)
-            }
-        }).build()
+            })
+            .addInterceptor(new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+            .build()
     }
 
     @Bean
     Retrofit cool() {
         new Retrofit.Builder().addConverterFactory(GsonConverterFactory.create(this.gson))
-                              .client(this.coolHttpClient())
-                              .baseUrl(this.coolQUrl)
-                              .build()
+            .client(this.coolHttpClient())
+            .baseUrl(this.coolQUrl)
+            .build()
     }
 
     @Bean
     OkHttpClient jiraHttpClient() {
-        new OkHttpClient.Builder().addInterceptor(new Interceptor() {
-            @Override
-            Response intercept(Interceptor.Chain chain) throws IOException {
-                // 添加授权token
-                Request request = chain.request()
-                                       .newBuilder()
-                                       .addHeader("Authorization", "Basic ${RetrofitConfig.this.jiraToken}")
-                                       .build()
-
-                log.debug("${request.headers()}")
-                // post日志记录
-                if (request.body()) {
-                    Buffer buffer = new Buffer()
-                    request.body().writeTo(buffer)
-                    log.debug(buffer.readString(Charset.forName("UTF-8")))
+        new OkHttpClient.Builder()
+            .addInterceptor(new Interceptor() {
+                @Override
+                Response intercept(Interceptor.Chain chain) throws IOException {
+                    // 添加授权token
+                    Request request = chain.request()
+                        .newBuilder()
+                        .addHeader("Authorization", "Basic ${RetrofitConfig.this.jiraToken}")
+                        .build()
+                    chain.proceed(request)
                 }
-
-                chain.proceed(request)
-            }
-        }).build()
+            })
+            .addInterceptor(new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+            .build()
     }
 
     @Bean
     Retrofit jira() {
         new Retrofit.Builder().addConverterFactory(GsonConverterFactory.create(this.gson))
-                              .client(this.jiraHttpClient())
-                              .baseUrl(this.jiraUrl)
-                              .build()
+            .client(this.jiraHttpClient())
+            .baseUrl(this.jiraUrl)
+            .build()
     }
 
     @Bean
@@ -125,48 +108,42 @@ class RetrofitConfig {
     @Bean
     Retrofit itpkRobot() {
         new Retrofit.Builder().addConverterFactory(GsonConverterFactory.create(this.gson))
-                              .baseUrl(this.itpkRobotUrl)
-                              .build()
+            .baseUrl(this.itpkRobotUrl)
+            .build()
     }
 
     @Bean
     Retrofit translate() {
         new Retrofit.Builder().addConverterFactory(GsonConverterFactory.create(this.gson))
-                              .baseUrl(this.translateUrl)
-                              .build()
+            .baseUrl(this.translateUrl)
+            .build()
     }
 
     @Bean
     OkHttpClient gitlabHttpClient() {
-        new OkHttpClient.Builder().addInterceptor(new Interceptor() {
-            @Override
-            Response intercept(Interceptor.Chain chain) throws IOException {
-                // 添加授权token
-                Request request = chain.request()
-                                       .newBuilder()
-                                       .addHeader("Content-Type", "application/json")
-                                       .addHeader("Private-Token", RetrofitConfig.this.gitlabToken)
-                                       .build()
-
-                log.debug("${request.headers()}")
-                // post日志记录
-                if (request.body()) {
-                    Buffer buffer = new Buffer()
-                    request.body().writeTo(buffer)
-                    log.debug(buffer.readString(Charset.forName("UTF-8")))
+        new OkHttpClient.Builder()
+            .addInterceptor(new Interceptor() {
+                @Override
+                Response intercept(Interceptor.Chain chain) throws IOException {
+                    // 添加授权token
+                    Request request = chain.request()
+                        .newBuilder()
+                        .addHeader("Content-Type", "application/json")
+                        .addHeader("Private-Token", RetrofitConfig.this.gitlabToken)
+                        .build()
+                    chain.proceed(request)
                 }
-
-                chain.proceed(request)
-            }
-        }).build()
+            })
+            .addInterceptor(new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+            .build()
     }
 
     @Bean
     Retrofit gitlab() {
         new Retrofit.Builder().addConverterFactory(GsonConverterFactory.create(this.gson))
-                              .client(this.gitlabHttpClient())
-                              .baseUrl(this.gitlabUrl)
-                              .build()
+            .client(this.gitlabHttpClient())
+            .baseUrl(this.gitlabUrl)
+            .build()
     }
 
 
